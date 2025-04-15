3836ddf293dfef06578772d924f02bfc798235f4
andersonkaiti <anderkaiti@gmail.com>
2025-04-15 20:46:29 -0300

    :lipstick: style fix: Corrigindo tamanho da tabela, além de seu esqueleto de carregamento, da página de dashboard.

--- clerk-blog/src/app/dashboard/loading.tsx
+++ clerk-blog/src/app/dashboard/loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
   return (
     <div className="flex min-h-[calc(100vh-5rem)] flex-col items-center justify-center">
-      <div className="w-fit overflow-x-auto p-5 shadow-md sm:rounded-lg">
+      <div className="w-full overflow-x-auto p-5 shadow-md sm:rounded-lg lg:w-fit">
         <div className="flex w-full animate-pulse flex-col gap-6 divide-gray-700 overflow-hidden border border-gray-700 bg-neutral-950 p-4 shadow-sm sm:rounded-lg">
           <div className="flex justify-between gap-10 border-b-1 border-gray-700 pb-4">
             {[...new Array(5)].map((_, index: number) => (
