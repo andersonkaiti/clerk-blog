0604bcfb68138226aab261363283e464138528a8
andersonkaiti <anderkaiti@gmail.com>
2025-04-16 19:18:49 -0300

    :heavy_plus_sign: build: Atualizando o build da API.

--- ws-clerk-blog/dist/server.js
+++ ws-clerk-blog/dist/server.js
@@ -1,6 +1,22 @@
+var __defProp = Object.defineProperty;
+var __defProps = Object.defineProperties;
+var __getOwnPropDescs = Object.getOwnPropertyDescriptors;
 var __getOwnPropSymbols = Object.getOwnPropertySymbols;
 var __hasOwnProp = Object.prototype.hasOwnProperty;
 var __propIsEnum = Object.prototype.propertyIsEnumerable;
+var __defNormalProp = (obj, key, value) => key in obj ? __defProp(obj, key, { enumerable: true, configurable: true, writable: true, value }) : obj[key] = value;
+var __spreadValues = (a, b) => {
+  for (var prop in b || (b = {}))
+    if (__hasOwnProp.call(b, prop))
+      __defNormalProp(a, prop, b[prop]);
+  if (__getOwnPropSymbols)
+    for (var prop of __getOwnPropSymbols(b)) {
+      if (__propIsEnum.call(b, prop))
+        __defNormalProp(a, prop, b[prop]);
+    }
+  return a;
+};
+var __spreadProps = (a, b) => __defProps(a, __getOwnPropDescs(b));
 var __objRest = (source, exclude) => {
   var target = {};
   for (var prop in source)
@@ -71,6 +87,13 @@ var PostRepository = class {
         ],
         where: {
           deleted: false
+        },
+        include: {
+          user: {
+            include: {
+              email_addresses: true
+            }
+          }
         }
       });
     });
@@ -134,6 +157,15 @@ var PostRepository = class {
       });
     });
   }
+  deleteUserPosts(userId) {
+    return __async(this, null, function* () {
+      yield this.database.posts.deleteMany({
+        where: {
+          userId
+        }
+      });
+    });
+  }
 };
 
 // config/database.ts
@@ -142,114 +174,463 @@ var database = new PrismaClient();
 
 // src/controllers/create-post-controller.ts
 var CreatePostController = class {
-  constructor(postRepository2) {
-    this.postRepository = postRepository2;
+  constructor(postRepository) {
+    this.postRepository = postRepository;
   }
   handle(req, res) {
     return __async(this, null, function* () {
-      const post = this.postRepository.create(req.body);
-      res.status(201).json(post);
+      try {
+        const post = this.postRepository.create(req.body);
+        res.status(201).json(post);
+      } catch (err) {
+        res.status(400).json({
+          err
+        });
+      }
     });
   }
 };
 
 // src/controllers/get-user-posts-controller.ts
 var GetUserPostsController = class {
-  constructor(postRepository2) {
-    this.postRepository = postRepository2;
+  constructor(postRepository) {
+    this.postRepository = postRepository;
   }
   handle(req, res) {
     return __async(this, null, function* () {
-      const { userId } = req.params;
-      const posts = yield this.postRepository.getByUserId(userId);
-      res.status(200).json(posts);
+      try {
+        const { userId } = req.params;
+        const posts = yield this.postRepository.getByUserId(userId);
+        res.status(200).json(posts);
+      } catch (err) {
+        res.status(400).json({
+          err
+        });
+      }
     });
   }
 };
 
 // src/controllers/get-posts-controller.ts
 var GetPostsController = class {
-  constructor(postRepository2) {
-    this.postRepository = postRepository2;
+  constructor(postRepository) {
+    this.postRepository = postRepository;
   }
   handle(_req, res) {
     return __async(this, null, function* () {
-      const posts = yield this.postRepository.get();
-      res.status(200).json(posts);
+      try {
+        const posts = yield this.postRepository.get();
+        res.status(200).json(posts);
+      } catch (err) {
+        res.status(400).json({
+          err
+        });
+      }
     });
   }
 };
 
 // src/controllers/get-post-by-id-controller.ts
 var GetPostByIdController = class {
-  constructor(postRepository2) {
-    this.postRepository = postRepository2;
+  constructor(postRepository) {
+    this.postRepository = postRepository;
   }
   handle(req, res) {
     return __async(this, null, function* () {
-      const { id } = req.params;
-      const post = yield this.postRepository.getById(id);
-      res.status(200).json(post);
+      try {
+        const { id } = req.params;
+        const post = yield this.postRepository.getById(id);
+        res.status(200).json(post);
+      } catch (err) {
+        res.status(400).json({
+          err
+        });
+      }
     });
   }
 };
 
 // src/controllers/update-post-controller.ts
 var UpdatePostController = class {
-  constructor(postRepository2) {
-    this.postRepository = postRepository2;
+  constructor(postRepository) {
+    this.postRepository = postRepository;
   }
   handle(req, res) {
     return __async(this, null, function* () {
-      const post = this.postRepository.update(req.body);
-      res.status(200).json(post);
+      try {
+        const post = this.postRepository.update(req.body);
+        res.status(200).json(post);
+      } catch (err) {
+        res.status(400).json({
+          err
+        });
+      }
     });
   }
 };
 
 // src/controllers/delete-post-controller.ts
 var DeletePostController = class {
-  constructor(postRepository2) {
-    this.postRepository = postRepository2;
+  constructor(postRepository) {
+    this.postRepository = postRepository;
   }
   handle(req, res) {
     return __async(this, null, function* () {
-      const { id } = req.params;
-      yield this.postRepository.delete(id);
-      res.status(200).json({
-        message: "Post deletado."
+      try {
+        const { id } = req.params;
+        yield this.postRepository.delete(id);
+        res.status(200).json({
+          message: "Post deletado."
+        });
+      } catch (err) {
+        res.status(400).json({
+          err
+        });
+      }
+    });
+  }
+};
+
+// src/factories/posts-controllers-factory.ts
+import { config } from "dotenv";
+config();
+var PostsControllersFactory = class {
+  createControllers() {
+    const postRepository = new PostRepository(database);
+    const createPostController2 = new CreatePostController(postRepository);
+    const getPostsController2 = new GetPostsController(postRepository);
+    const getUserPostsController2 = new GetUserPostsController(postRepository);
+    const getPostByIDController2 = new GetPostByIdController(postRepository);
+    const updatePostController2 = new UpdatePostController(postRepository);
+    const deletePostController2 = new DeletePostController(postRepository);
+    return {
+      createPostController: createPostController2,
+      getPostsController: getPostsController2,
+      getUserPostsController: getUserPostsController2,
+      getPostByIDController: getPostByIDController2,
+      updatePostController: updatePostController2,
+      deletePostController: deletePostController2
+    };
+  }
+};
+
+// src/routes/post-routes.ts
+var {
+  createPostController,
+  deletePostController,
+  getPostByIDController,
+  getPostsController,
+  getUserPostsController,
+  updatePostController
+} = new PostsControllersFactory().createControllers();
+var router2 = Router2();
+router2.post("/", createPostController.handle.bind(createPostController));
+router2.put("/", updatePostController.handle.bind(updatePostController));
+router2.delete("/:id", deletePostController.handle.bind(deletePostController));
+router2.get("/", getPostsController.handle.bind(getPostsController));
+router2.get(
+  "/by-user-id/:userId",
+  getUserPostsController.handle.bind(getUserPostsController)
+);
+router2.get(
+  "/by-post-id/:id",
+  getPostByIDController.handle.bind(getPostByIDController)
+);
+
+// src/routes/user-routes.ts
+import { Router as Router3 } from "express";
+
+// src/repositories/user-repository.ts
+var UserRepository = class {
+  constructor(database2) {
+    this.database = database2;
+  }
+  create(_a) {
+    return __async(this, null, function* () {
+      var _b = _a, { email_addresses } = _b, user = __objRest(_b, ["email_addresses"]);
+      yield this.database.users.create({
+        data: __spreadProps(__spreadValues({}, user), {
+          email_addresses: {
+            create: email_addresses.map((email) => ({
+              id: email.id,
+              email_address: email.email_address,
+              object: email.object
+            }))
+          }
+        })
+      });
+    });
+  }
+  update(_c) {
+    return __async(this, null, function* () {
+      var _d = _c, { email_addresses } = _d, user = __objRest(_d, ["email_addresses"]);
+      yield this.database.users.update({
+        where: {
+          id: user.id
+        },
+        data: __spreadProps(__spreadValues({}, user), {
+          email_addresses: {
+            deleteMany: {},
+            create: email_addresses.map((email) => ({
+              id: email.id,
+              email_address: email.email_address,
+              object: email.object
+            }))
+          }
+        })
+      });
+    });
+  }
+  delete(id) {
+    return __async(this, null, function* () {
+      yield this.database.users.delete({
+        include: {
+          email_addresses: true
+        },
+        where: {
+          id
+        }
       });
     });
   }
 };
 
-// src/routes/post-routes.ts
-var router2 = Router2();
-var postRepository = new PostRepository(database);
-var createPostController = new CreatePostController(postRepository);
-var getPostsController = new GetPostsController(postRepository);
-var getUserPostsController = new GetUserPostsController(postRepository);
-var getPostByIDController = new GetPostByIdController(postRepository);
-var updatePostController = new UpdatePostController(postRepository);
-var deletePostController = new DeletePostController(postRepository);
-router2.post("/", (req, res) => {
-  createPostController.handle(req, res);
-});
-router2.put("/", (req, res) => {
-  updatePostController.handle(req, res);
-});
-router2.delete("/:id", (req, res) => {
-  deletePostController.handle(req, res);
-});
-router2.get("/", (req, res) => {
-  getPostsController.handle(req, res);
-});
-router2.get("/by-user-id/:userId", (req, res) => {
-  getUserPostsController.handle(req, res);
-});
-router2.get("/by-post-id/:id", (req, res) => {
-  getPostByIDController.handle(req, res);
-});
+// src/services/clerk-webhook.ts
+var ClerkWeebhookService = class {
+  constructor(webhook) {
+    this.webhook = webhook;
+  }
+  verify(req) {
+    return __async(this, null, function* () {
+      const {
+        "svix-id": svix_id,
+        "svix-timestamp": svix_timestamp,
+        "svix-signature": svix_signature
+      } = req.headers;
+      if (!svix_id || !svix_timestamp || !svix_signature) {
+        throw new Error("Headers svix ausentes.");
+      }
+      const payload = JSON.stringify(req.body);
+      const headers = {
+        "svix-id": svix_id,
+        "svix-timestamp": svix_timestamp,
+        "svix-signature": svix_signature
+      };
+      try {
+        return this.webhook.verify(payload, headers);
+      } catch (err) {
+        if (err instanceof Error) {
+          throw new Error(err.message);
+        }
+      }
+    });
+  }
+};
+
+// src/factories/clerk-webhook-service-factory.ts
+import { Webhook } from "svix";
+var ClerkWeebhookServiceFactory = class {
+  createWebhook(secret) {
+    return {
+      clerkWebhookService: new ClerkWeebhookService(new Webhook(secret))
+    };
+  }
+};
+
+// src/controllers/create-user-controller.ts
+var CreateUserController = class {
+  constructor(userRepository, clerkWebhookService) {
+    this.userRepository = userRepository;
+    this.clerkWebhookService = clerkWebhookService;
+  }
+  handle(req, res) {
+    return __async(this, null, function* () {
+      try {
+        const {
+          type: eventType,
+          data: {
+            created_at,
+            last_sign_in_at,
+            updated_at,
+            email_addresses,
+            id,
+            first_name,
+            image_url,
+            last_name,
+            profile_image_url,
+            username
+          }
+        } = yield this.clerkWebhookService.verify(req);
+        if (eventType === "user.created") {
+          yield this.userRepository.create({
+            created_at: new Date(created_at),
+            last_sign_in_at: new Date(last_sign_in_at),
+            updated_at: new Date(updated_at),
+            email_addresses,
+            id,
+            first_name,
+            image_url,
+            last_name,
+            profile_image_url,
+            username
+          });
+          res.status(201).json({
+            message: "Usu\xE1rio criado com sucesso!"
+          });
+        }
+      } catch (err) {
+        if (err instanceof Error) {
+          res.status(400).json({
+            message: "Falha na verifica\xE7\xE3o do webhook.",
+            error: err.message
+          });
+        }
+      }
+    });
+  }
+};
+
+// src/controllers/update-user-controller.ts
+var UpdateUserController = class {
+  constructor(userRepository, clerkWebhookService) {
+    this.userRepository = userRepository;
+    this.clerkWebhookService = clerkWebhookService;
+  }
+  handle(req, res) {
+    return __async(this, null, function* () {
+      try {
+        const {
+          data: {
+            created_at,
+            last_sign_in_at,
+            updated_at,
+            birthday,
+            email_addresses,
+            id,
+            first_name,
+            image_url,
+            last_name,
+            profile_image_url,
+            username
+          },
+          type: eventType
+        } = yield this.clerkWebhookService.verify(req);
+        if (eventType === "user.updated") {
+          yield this.userRepository.update({
+            created_at: new Date(created_at),
+            last_sign_in_at: new Date(last_sign_in_at),
+            updated_at: new Date(updated_at),
+            birthday,
+            email_addresses,
+            id,
+            first_name,
+            image_url,
+            last_name,
+            profile_image_url,
+            username
+          });
+          res.status(200).json({
+            message: "Usu\xE1rio atualizado com sucesso!"
+          });
+        }
+      } catch (err) {
+        if (err instanceof Error) {
+          res.status(400).json({
+            message: "Falha na verifica\xE7\xE3o do webhook.",
+            error: err.message
+          });
+        }
+      }
+    });
+  }
+};
+
+// src/controllers/delete-user-posts.ts
+var DeleteUserPostsControler = class {
+  constructor(userRepository, clerkWebhookService) {
+    this.userRepository = userRepository;
+    this.clerkWebhookService = clerkWebhookService;
+  }
+  handle(req, res) {
+    return __async(this, null, function* () {
+      try {
+        const {
+          type: eventType,
+          data: { id }
+        } = yield this.clerkWebhookService.verify(req);
+        if (eventType === "user.deleted") {
+          yield this.userRepository.delete(id);
+          res.status(200).json({
+            message: "Posts e usu\xE1rio deletados com sucesso!"
+          });
+        }
+        res.status(200).json({
+          message: "Evento n\xE3o tratado."
+        });
+      } catch (err) {
+        res.status(400).json({
+          message: "Falha na verifica\xE7\xE3o do webhook.",
+          err
+        });
+      }
+    });
+  }
+};
+
+// src/factories/user-controllers-factory.ts
+import { config as config2 } from "dotenv";
+config2();
+var UserControllersFactory = class {
+  createController() {
+    const { clerkWebhookService: clerkWebhookCreateUserService } = new ClerkWeebhookServiceFactory().createWebhook(
+      process.env.CREATE_USER_SECRET
+    );
+    const { clerkWebhookService: clerkWebhookUpdateUserService } = new ClerkWeebhookServiceFactory().createWebhook(
+      process.env.UPDATE_USER_SECRET
+    );
+    const { clerkWebhookService: clerkWebhookDeleteUserService } = new ClerkWeebhookServiceFactory().createWebhook(
+      process.env.DELETE_USER_SECRET
+    );
+    const userRepository = new UserRepository(database);
+    const createUserController2 = new CreateUserController(
+      userRepository,
+      clerkWebhookCreateUserService
+    );
+    const updateUserController2 = new UpdateUserController(
+      userRepository,
+      clerkWebhookUpdateUserService
+    );
+    const deleteUserPostsController2 = new DeleteUserPostsControler(
+      userRepository,
+      clerkWebhookDeleteUserService
+    );
+    return {
+      createUserController: createUserController2,
+      updateUserController: updateUserController2,
+      deleteUserPostsController: deleteUserPostsController2
+    };
+  }
+};
+
+// src/routes/user-routes.ts
+var router3 = Router3();
+var {
+  createUserController,
+  updateUserController,
+  deleteUserPostsController
+} = new UserControllersFactory().createController();
+router3.post(
+  "/api/webhooks/created-user",
+  createUserController.handle.bind(createUserController)
+);
+router3.post(
+  "/api/webhooks/updated-user",
+  updateUserController.handle.bind(updateUserController)
+);
+router3.post(
+  "/api/webhooks/deleted-user",
+  deleteUserPostsController.handle.bind(deleteUserPostsController)
+);
 
 // src/app.ts
 var app = express();
@@ -258,6 +639,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use("/", router);
 app.use("/post", router2);
+app.use("/user", router3);
 
 // bin/server.ts
 var port = 4e3;
