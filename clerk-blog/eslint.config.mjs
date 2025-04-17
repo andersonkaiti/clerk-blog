cf30a239213f97289a0122e1358c37bd445515ec
andersonkaiti <anderkaiti@gmail.com>
2025-04-17 08:44:51 -0300

    :wrench: chore: Configurando o plugin eslint-plugin-import-helpers.

--- clerk-blog/eslint.config.mjs
+++ clerk-blog/eslint.config.mjs
@@ -1,6 +1,7 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import importHelpers from "eslint-plugin-import-helpers";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +12,39 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: {
+      "import-helpers": importHelpers,
+    },
+    rules: {
+      "import-helpers/order-imports": [
+        "warn",
+        {
+          newlinesBetween: "never",
+
+          groups: [
+            ["/^react/"],
+            "module",
+            "/^@components/",
+            "/^@pages/",
+            "/^@actions/",
+            "/^@services/",
+            "/^@adapters/",
+            "/^types/",
+            "/^@hooks/",
+            "/^@validators/",
+            "/^@config/",
+            ["parent", "sibling", "index"],
+          ],
+
+          alphabetize: {
+            order: "asc",
+            ignoreCase: true,
+          },
+        },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
