eb5ee9fa8ac23c588d2f1559422f48497bf9901b
andersonkaiti <anderkaiti@gmail.com>
2025-04-19 10:47:28 -0300

    :lipstick: style: Melhoria na estilização da página inicial.

--- clerk-blog/src/app/(home)/loading.tsx
+++ clerk-blog/src/app/(home)/loading.tsx
@@ -1,19 +1,24 @@
+import { DottedBackground } from "@components/dotted-background";
+
 export default function Loading() {
   return (
-    <div className="flex min-h-[calc(100vh-5rem)] items-center justify-center">
-      <div className="grid h-full w-full grid-cols-1 gap-4 p-4 sm:grid-cols-2">
-        {[...new Array(9)].map((_, index: number) => (
-          <div
-            key={index}
-            role="status"
-            className="h-[162px] w-full animate-pulse space-y-4 divide-gray-700 overflow-hidden rounded-lg border border-gray-700 p-6 shadow-sm"
-          >
-            <div className="mb-2 h-8 w-70 rounded-full bg-gray-600" />
-            <div className="mb-3 h-6 w-50 rounded-full bg-gray-600" />
-            <div className="h-9 w-33 rounded-lg bg-gray-600 px-3 py-2" />
-          </div>
-        ))}
+    <DottedBackground>
+      <div className="flex min-h-[calc(100vh-5rem)] items-center justify-center">
+        <div className="grid h-full w-full max-w-7xl grid-cols-1 gap-4 p-10 sm:grid-cols-2">
+          {[...new Array(8)].map((_, index: number) => (
+            <div
+              key={index}
+              role="status"
+              className="h-42.5 w-full animate-pulse space-y-2 divide-gray-700 overflow-hidden rounded-lg border border-gray-700 p-4 shadow-sm"
+            >
+              <div className="mb-3 h-4 w-30 rounded-full bg-gray-600" />
+              <div className="mb-2 h-8 w-2/3 rounded-full bg-gray-600" />
+              <div className="mb-3 h-6 w-1/2 rounded-full bg-gray-600" />
+              <div className="h-9 w-33 rounded-lg bg-gray-600 px-3 py-2" />
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </DottedBackground>
   );
 }
--- clerk-blog/src/app/(home)/page.tsx
+++ clerk-blog/src/app/(home)/page.tsx
@@ -1,5 +1,6 @@
 export const dynamic = "force-dynamic";
 
+import { DottedBackground } from "@components/dotted-background";
 import { PostCard } from "@components/post-card";
 import { getAllPosts } from "@actions/get-all-posts";
 import { IPost } from "types/user-post";
@@ -8,12 +9,14 @@ export default async function Home() {
   const posts: IPost[] = await getAllPosts();
 
   return (
-    <div className="flex min-h-[calc(100vh-5rem)] flex-col items-center justify-center space-y-4">
-      <div className="grid w-full grid-cols-1 gap-4 p-4 sm:grid-cols-2">
-        {posts.map((post: IPost, index: number) => (
-          <PostCard post={post} key={index} />
-        ))}
+    <DottedBackground>
+      <div className="flex min-h-[calc(100vh-5rem)] flex-col items-center justify-center space-y-4">
+        <div className="grid w-full max-w-7xl grid-cols-1 gap-4 p-4 sm:grid-cols-2 sm:p-10">
+          {posts.map((post: IPost, index: number) => (
+            <PostCard post={post} key={index} />
+          ))}
+        </div>
       </div>
-    </div>
+    </DottedBackground>
   );
 }
