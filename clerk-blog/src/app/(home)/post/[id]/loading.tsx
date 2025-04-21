440e47c21fd8de1d07f4ae77a591a1785002d2b4
andersonkaiti <anderkaiti@gmail.com>
2025-04-20 23:18:07 -0300

    :lipstick: style: Correção do uso de um skeleton a partir de sua substituição por um border-b.

--- clerk-blog/src/app/(home)/post/[id]/loading.tsx
+++ clerk-blog/src/app/(home)/post/[id]/loading.tsx
@@ -1,17 +1,16 @@
 export default function Loading() {
   return (
-    <div className="mx-auto flex min-h-[calc(100vh-5rem)] max-w-5xl flex-col items-center justify-center space-y-4 p-8">
+    <div className="mx-auto min-h-[calc(100vh-5rem)] max-w-5xl space-y-4 p-8">
       <div className="w-full space-y-8">
         <div className="h-6 w-18.5 rounded-full bg-gray-600" />
         <div className="h-4 w-43 rounded-full bg-gray-600" />
         <div className="h-12 w-43 rounded-full bg-gray-600" />
-        <div className="space-y-2">
+        <div className="space-y-2 border-b border-b-neutral-700 pb-8">
           <div className="h-4 w-18 rounded-full bg-gray-600" />
           <div className="flex items-center gap-1">
             <div className="h-6 w-6 rounded-full bg-gray-600" />
             <div className="h-4 w-14 rounded-full bg-gray-600" />
           </div>
-          <div className="mt-8 h-0.5 w-full rounded-full bg-gray-600" />
         </div>
         <div className="space-y-1">
           <div className="h-6 w-full rounded-full bg-gray-600" />
