63259e1092de28a8cb5feefc1b771fa9da4ac21a
andersonkaiti <anderkaiti@gmail.com>
2025-04-23 10:22:54 -0300

    :heavy_plus_sign: build: Adição do @tanstack/react-query-devtools.

--- clerk-blog/src/contexts/query-provider.tsx
+++ clerk-blog/src/contexts/query-provider.tsx
@@ -2,11 +2,17 @@
 
 import { ReactNode } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
 export function QueryProvider({ children }: { children: ReactNode }) {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <div className="fixed z-50">
+        <ReactQueryDevtools position="right" />
+      </div>
+    </QueryClientProvider>
   );
 }
