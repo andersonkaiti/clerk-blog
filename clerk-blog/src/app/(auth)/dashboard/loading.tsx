7bcfac93477c1c71766c0497891f229cde44e6de
andersonkaiti <anderkaiti@gmail.com>
2025-04-23 08:56:18 -0300

    :lipstick: style: Adição da classe animate-pulse nos esqueletos da página inicial e de dashboard.

--- clerk-blog/src/app/(auth)/dashboard/loading.tsx
+++ clerk-blog/src/app/(auth)/dashboard/loading.tsx
@@ -2,7 +2,7 @@ export default function Loading() {
   return (
     <div className="flex min-h-[calc(100vh-5rem)] flex-col items-center">
       <div className="sm:rounded-lgx w-full overflow-x-auto p-5 shadow-md">
-        <div className="flex items-center justify-between gap-2 py-4">
+        <div className="flex animate-pulse items-center justify-between gap-2 py-4">
           <div className="h-7.5 w-54 rounded-lg bg-gray-600" />
           <div className="h-9 w-29.5 rounded-lg bg-gray-600 px-3 py-2" />
         </div>
