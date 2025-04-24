6143c615e70aa2ad5484565ce1ae6b17730f9677
andersonkaiti <anderkaiti@gmail.com>
2025-04-24 09:40:15 -0300

    :sparkles: feat: Implementação de atualização otimista quando o usuário deleta o post.

--- clerk-blog/src/hooks/get-user-posts.hook.ts
+++ clerk-blog/src/hooks/get-user-posts.hook.ts
@@ -1,15 +1,49 @@
 "use client";
 
+import { startTransition, useOptimistic } from "react";
 import { useAuth } from "@clerk/nextjs";
-import { useSuspenseQuery } from "@tanstack/react-query";
+import { useQueryClient, useSuspenseQuery } from "@tanstack/react-query";
+import { toast } from "sonner";
+import { deletePost } from "@actions/delete-post";
 import { getUserPosts } from "@services/get-user-posts";
+import { IPost } from "types/user-post";
 
 export function useGetUserPosts(filter: string) {
   const { userId } = useAuth();
 
-  return useSuspenseQuery({
-    queryKey: [userId, filter || ""],
+  const queryClient = useQueryClient();
+
+  const QUERY_KEY = [userId, filter || ""];
+
+  const { data: posts, ...rest } = useSuspenseQuery({
+    queryKey: QUERY_KEY,
     queryFn: getUserPosts,
     staleTime: 0,
   });
+
+  const [optimisticPosts, setOptimisticPosts] = useOptimistic(
+    posts,
+    (currentPosts: IPost[], postId: string) =>
+      currentPosts.filter((post) => post.id !== postId),
+  );
+
+  async function handleDeletePost(post: IPost) {
+    startTransition(() => {
+      setOptimisticPosts(post.id);
+    });
+
+    await deletePost(post.userId, post.id);
+
+    queryClient.invalidateQueries({
+      queryKey: QUERY_KEY,
+    });
+
+    toast.success("Post deletado com sucesso!");
+  }
+
+  return {
+    optimisticPosts,
+    handleDeletePost,
+    ...rest,
+  };
 }
