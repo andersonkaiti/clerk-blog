61f79ff4c1ce5378cb0a416c1b2610e957d6484b
andersonkaiti <anderkaiti@gmail.com>
2025-04-27 12:17:56 -0300

    :lipstick: style: Alteração no estilo dos botões que levam para a mesma URL em que o usuário já está.

--- clerk-blog/src/components/pagination/pagination-button.tsx
+++ clerk-blog/src/components/pagination/pagination-button.tsx
@@ -1,5 +1,9 @@
+"use client";
+
 import { ReactNode } from "react";
+import { cn } from "@utils/cn";
 import Link, { LinkProps } from "next/link";
+import { useSearchParams } from "next/navigation";
 
 export interface IPaginationButtonProps extends LinkProps {
   children: ReactNode;
@@ -7,12 +11,21 @@ export interface IPaginationButtonProps extends LinkProps {
 
 export function PaginationButton({
   children,
+  href,
   ...props
 }: IPaginationButtonProps) {
+  const page = useSearchParams().get("page");
+
+  const isDisabled = page === String(href).split("=")[1];
+
   return (
     <Link
-      className="focus:ring-neutral-850 inline-flex h-10 w-10 items-center justify-center rounded-lg border border-gray-700 bg-black px-3 py-2 text-center text-sm font-medium text-white transition-colors hover:bg-neutral-700 focus:ring-4 focus:ring-neutral-600 focus:outline-none"
+      className={cn(
+        "focus:ring-neutral-850 inline-flex h-10 w-10 items-center justify-center rounded-lg border border-gray-700 bg-black px-3 py-2 text-center text-sm font-medium text-white transition-colors hover:bg-neutral-700 focus:ring-neutral-600 focus:outline-none",
+        isDisabled ? "focus:ring-none hover:bg-black" : "focus:ring-4",
+      )}
       {...props}
+      href={href}
     >
       {children}
     </Link>
